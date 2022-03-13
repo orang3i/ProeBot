afd85ddc629aa0c1d8e1795d87a6372fa06b344e
orang3i <86411355+orang3i@users.noreply.github.com>
2022-03-13 14:43:59 +0530

    many changed lmao

--- src/main/java/com/orang3i/discord/MessageListener.java
+++ src/main/java/com/orang3i/discord/MessageListener.java
@@ -4,13 +4,13 @@ import com.orang3i.ProeBot;
 import com.orang3i.config.DiscordConfig;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.util.List;
 
 public class MessageListener extends ListenerAdapter {
 
@@ -32,7 +32,7 @@ public class MessageListener extends ListenerAdapter {
         } catch (final LoginException | InterruptedException e) {
             throw new IllegalStateException("Could not login to Discord, check your bot-token in discord.yml",e);
         }
-        jda.getPresence().setActivity(Activity.playing("Type !ping"));
+        jda.getPresence().setActivity(Activity.watching("how to become proe by ChongJF"));
         jda.addEventListener(new MessageListener());
     }
     @Override
@@ -49,10 +49,41 @@ public class MessageListener extends ListenerAdapter {
                         response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
                     });
         }
+        String[] words = msg.getContentRaw().toString().split("\\s+");
+        int length = words.length;
+        try {
+            if (((msg.getContentRaw().substring(0, msg.getContentRaw().indexOf(' '))).equals(prefix + "whois"))&& length > 1) {
 
-        if(msg.getContentRaw().equals(prefix+"whoami")){
+
+
+
+                if (length > 1) {
+
+                    String i = msg.getMentionedUsers().get(0).getName();
+
+                    if(i != null) {
+                        msg.getChannel().sendMessage("they are " + i + " they created their account on: " + msg.getMentionedUsers().get(0).getTimeCreated().toString().substring(0, msg.getMentionedUsers().get(0).getTimeCreated().toString().length() - 14)).queue();
+                    }
+                }
+            }
+        }catch (Exception e){}
+
+        if(msg.getContentRaw().equals(prefix+"whois")){
+
             String i = msg.getAuthor().getName();
-            msg.getChannel().sendMessage("you are "+i).queue();
+            //get message author join date
+
+
+
+            msg.getChannel().sendMessage("you are " + i + " you created your account on: " + msg.getAuthor().getTimeCreated().toString().substring(0, msg.getAuthor().getTimeCreated().toString().length() - 14)).queue();
         }
+        try {
+            if (((msg.getContentRaw().substring(0, msg.getContentRaw().indexOf(' '))).equals(prefix + "whoid")) && length > 1) {
+                String str = msg.getContentRaw();
+                String[] sp = str.split(" ");
+                Long id = Long.parseLong(sp[1]);
+                msg.getChannel().sendMessage("the id is " + id).queue();
+            }
+        }catch (Exception e){}
     }
 }
